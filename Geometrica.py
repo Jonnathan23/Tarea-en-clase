3edd08536b93b09969393555540f64d0dbc7f40d
Jonnathan <jsaquicela26@gmail.com>
2024-04-17 18:58:38 -0500

    Implementacion de gráfica en la distribucion geometrica

--- Geometrica.py
+++ Geometrica.py
@@ -1,8 +1,23 @@
-#Inicialización de variables
+import matplotlib.pyplot as plt
+
+# Inicialización de variables
 p = float(input("Ingrese la probabilidad de falla % ")) / 100
-q = 1- p
+q = 1 - p
 x = int(input("Numero de la prueba en la que ocurre el exito "))
 
-px = p*(1-p)**(x-1)
+# Cálculo de la probabilidad
+px = p * (1 - p) ** (x - 1)
 
-print(f'La probabilidad de fallo en el intento {x} es de: {px*100} %')
+# Creación de la lista de valores de x
+xValores = list(range(1, x + 1))
+
+# Creación de la lista de probabilidades
+probabilidades = [p * (1 - p) ** (i - 1) for i in xValores]
+
+# Gráfico de barras
+plt.bar(xValores, probabilidades, label=f'Probabilidad: {px*100:.2f}%')
+plt.xlabel('Número de intento')
+plt.ylabel('Probabilidad')
+plt.title('Distribución de probabilidad binomial')
+plt.legend()
+plt.show()
